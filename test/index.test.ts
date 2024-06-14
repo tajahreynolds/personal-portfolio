6e2bcfb677e24ba6e242272db2afa692f0e30b77
TaJah Reynolds <tajahreynolds22@gmail.com>
2024-06-13 21:36:27 -0400

    add bun types

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ import { hono } from 'index';
 
 describe("Server", () => {
   it("responds", async () => {
-    const req = new Request('http://127.0.0.1/')
+    const req = new Request('http://localhost/')
     const res = await hono.fetch(req);
     expect(res.status).toBe(200);
   });
